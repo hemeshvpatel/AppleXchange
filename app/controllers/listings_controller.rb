c861178fa346435f1e8ea96ec92c3978083d7ef3
hemeshvpatel <hemesh.v.patel@gmail.com>
2019-09-10 15:56:24 -0500

    have basic form set up, now need to make sure it works

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,16 @@
 class ListingsController < ApplicationController
+    def new_phone
+        @listing = Listing.new
+    end
+
+    def create
+        @listing = Listing.create(params[listing_params])
+    end
+
+    private
+
+    def listing_params
+        params.require(:listing).permit(:user_id, :product_id, :condition, :color, :storage, :memory, :price, :active)
+    end
+
 end
