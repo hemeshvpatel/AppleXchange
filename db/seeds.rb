8cfde1311fa7a7971e6370407e29424b0ac90452
hemeshvpatel <hemesh.v.patel@gmail.com>
2019-09-09 20:33:43 -0500

    added seed data for Types and Products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#Creating types of products:
+type1 = Type.create(name: "iPhone")
+type2 = Type.create(name: "MacBook")
+
+#Creating all phone products that users can select from:
+iphone_X = Product.create(name: "iPhone X", type_id: 1, model: "A1X00")
+iphone_XR = Product.create(name: "iPhone XR", type_id: 1, model: "A1XR0")
+iphone_XS = Product.create(name: "iPhone XS", type_id: 1, model: "A1XS0")
+iphone_XS_MAX = Product.create(name: "iPhone XS Max", type_id: 1, model: "A1XSM")
+
+#Creating all laptop products that users can select from:
+macBook_12 = Product.create(name: "MacBook ", type_id: 2, model: "AMB12")
+macBook_Pro_13 = Product.create(name: "MacBook Pro 13inch", type_id: 2, model: "AMB13")
+macBook_Pro_15 = Product.create(name: "MacBook Pro 15inch", type_id: 2, model: "AMB15")
+macBook_Air = Product.create(name: "MacBook Air", type_id: 2, model: "AMBAI")
