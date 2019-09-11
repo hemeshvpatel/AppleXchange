39f82f94f0d1b02a760ae85644aa086e24c70738
hemeshvpatel <hemesh.v.patel@gmail.com>
2019-09-11 13:25:09 -0500

    ability to edit forms is now working

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,13 @@ class Product < ApplicationRecord
     has_many :listings
     has_many :users, through: :listings
     has_many :reviews
+
+    def active_listings
+        self.listings.select {|listing| listing.active == true}
+    end
+
+    def inactive_listings
+        self.listings.select {|listing| listing.active == false}
+    end
+
 end
